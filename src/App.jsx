4047e9459d9bfa881d47a4278f6f57f344fb6081
adanso-su <akosua.k.danso@gmail.com>
2025-04-11 19:26:19 -0400

    ...

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import React from 'react';
+import SignIn from "./signIn";
+
+function App() {
+  const [currentForm, setCurrent] = useState('login');
+  return (
+    <SignIn/>
+  )
+}
+
+export default App;
