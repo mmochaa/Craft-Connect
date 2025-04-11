49d9b18bed68adbde2e25dce2674dd10421425cd
mocha <114508094+mmochaa@users.noreply.github.com>
2025-04-11 18:51:29 -0400

    Update App.jsx
    added main.jsx stuff

--- App.jsx
+++ App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import React from 'react';
 import SignIn from "./signIn";
-
-
-
+import Main from "./Main";
 
 function App() {
-const [currentForm, setCurrent] = useState('login');
+  const [currentForm, setCurrent] = useState('login');
   return (
-    <SignIn/>
-  )
+    <div>
+      {currentForm === 'login' ? <SignIn /> : <Main />}
+    </div>
+  );
 }
 
-export default App
+export default App;
